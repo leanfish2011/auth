9ba4e848c28aa971f24aab80374e7562775b04bc
yangtzeyufei <yangtze_yufei@163.com>
2020-04-06 18:16:37 +0800

    feat(docker):镜像加上分支版本

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -7,6 +7,9 @@
 # 整个项目父路径
 project_path_prefix="/home/tim/git/ishou"
 
+# 版本
+version="v1.0"
+
 # 生成的jar包后缀
 jar_name_suffix="0.0.1-SNAPSHOT.jar"
 
@@ -18,16 +21,20 @@ function build_image()
     echo $jar_name
     cd $project_path
     latest_commit_id=$(git rev-parse --short HEAD)
+    branch=$(git symbolic-ref --short -q HEAD)
+
     cd $project_path"/"$2
     rm -rf target
     cd $project_path"/"$3
     rm -rf target
+
     cd ../
     mvn package
     cp $project_path"/"$3"/target/"$jar_name $project_path"/docker"
     cd $project_path"/docker"
+
     time=$(date "+%Y%m%d_%H%M%S")
-    tag=$time"_"$latest_commit_id
+    tag=$version"_"$branch"_"$time"_"$latest_commit_id
     docker_name=$1":"$tag
     sudo docker build -t $docker_name .
 }
