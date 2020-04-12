ac7eb73a8ab72dd744d5e7941d971d9f4107470f
yangtzeyufei <yangtze_yufei@163.com>
2020-04-12 14:19:18 +0800

    feat(login):集成github登录

--- auth-server/src/main/java/com/tim/auth/constant/GitHubLoginConstant.java
+++ auth-server/src/main/java/com/tim/auth/constant/GitHubLoginConstant.java
@@ -13,15 +13,13 @@ public class GitHubLoginConstant {
   //这里填写在GitHub上注册应用时候获得 CLIENT_SECRET
   public static final String CLIENT_SECRET = "21c9e61ed469c1abf87f8cdc29c9a59c8b0bdeb8";
 
-  // 回调路径
-  public static final String CALLBACK = "http://localhost:8080/github/auth";
-
   //获取token的url
   public static final String TOKEN_URL =
       "https://github.com/login/oauth/access_token?client_id=" + CLIENT_ID + "&client_secret="
-          + CLIENT_SECRET + "&code=CODE&redirect_uri=" + CALLBACK + "";
+          + CLIENT_SECRET + "&code=CODE";
 
   //获取用户信息的url
-  public static final String USER_INFO_URL = "https://api.github.com/user?access_token=TOKEN";
+  public static final String
+      USER_INFO_URL = "https://api.github.com/user?access_token=TOKEN";
 
 }
