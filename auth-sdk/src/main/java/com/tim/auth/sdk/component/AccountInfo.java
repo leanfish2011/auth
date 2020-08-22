273e20ecb4f7b94f2a962b16d736c073f87cde4b
yangtzeyufei <yangtze_yufei@163.com>
2020-08-22 17:25:46 +0800

    feat(account):提取子工程公共类

---
+++ auth-sdk/src/main/java/com/tim/auth/sdk/component/AccountInfo.java
@@ -0,0 +1,34 @@
+package com.tim.auth.sdk.component;
+
+import com.tim.auth.sdk.vo.TokenModel;
+import com.tim.exception.type.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author：tim
+ * @date：20-4-11 上午11:27
+ * @description： 存储当前用户信息
+ */
+@Component
+@Lazy
+@Slf4j
+public class AccountInfo {
+
+  private TokenModel tokenModel;
+
+  public TokenModel getUserInfo() {
+    if (tokenModel == null) {
+      log.warn("未找到用户");
+      throw new NotFoundException("未找到用户");
+    }
+
+    return tokenModel;
+  }
+
+  public void setTokenModel(TokenModel tokenModel) {
+    this.tokenModel = tokenModel;
+  }
+
+}
