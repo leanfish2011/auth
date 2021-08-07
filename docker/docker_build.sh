fb2fb8be28e054d3b54420056d4ce8bc54fcbcf9
yangtzeyufei <yangtze_yufei@163.com>
2021-08-07 21:57:25 +0800

    feat(docker):固定jar名称

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -8,45 +8,63 @@
 project_path_prefix="/home/tim/git/ishou"
 
 # 版本
-version="v1.0"
+version="v2.0"
 
 # 生成的jar包后缀
-jar_name_suffix="0.0.1-SNAPSHOT.jar"
-
-function build_image()
-{
-    project_path=$project_path_prefix"/"$1
-    jar_name=$3"-"$jar_name_suffix
-    echo $project_path
-    echo $jar_name
-    cd $project_path
-    latest_commit_id=$(git rev-parse --short HEAD)
-    branch=$(git symbolic-ref --short -q HEAD)
-
-    cd $project_path"/"$2
-    rm -rf target
-    cd $project_path"/"$3
-    rm -rf target
-
-    cd ../
-    mvn package
-    cp $project_path"/"$3"/target/"$jar_name $project_path"/docker"
-    cd $project_path"/docker"
-
-    time=$(date "+%Y%m%d_%H%M%S")
-    tag=$version"_"$branch"_"$time"_"$latest_commit_id
-    docker_name=$1":"$tag
-    sudo docker build -t $docker_name .
-}
-
-function test(){
-    echo $project_path_prefix
-}
+jar_name_suffix=".jar"
 
 # 根据各个项目修改
 project_name="auth"
 sdk_name="auth-sdk"
 service_name="auth-server"
+
+function build_image()
+{
+    project_path=$project_path_prefix"/"$1
+    echo "当前项目路径："$project_path
+
+    jar_name=$3""$jar_name_suffix
+    echo "待生成镜像的jar包："$jar_name
+
+    cd $project_path
+
+    # 获取当前分支最后commit
+    latest_commit_id=$(git rev-parse --short HEAD)
+
+    # 获取当前分支名称
+    branch=$(git symbolic-ref --short -q HEAD)
+
+    # 清理target
+    cd $project_path"/"$2
+    rm -rf target
+
+    cd $project_path"/"$3
+    rm -rf target
+
+    # 构建
+    cd ../
+    mvn package
+
+    # 将jar包拷贝到docker目录中
+    cp $project_path"/"$3"/target/"$jar_name $project_path"/docker"
+    cd $project_path"/docker"
+
+    # 构建镜像名称 工程名称:版本号_分支名称_日期_时间_commitid
+    time=$(date "+%Y%m%d_%H%M%S")
+    tag=$version"_"$branch"_"$time"_"$latest_commit_id
+    docker_name=$1":"$tag
+
+    # 执行Dockerfile生成镜像
+    sudo docker build -t $docker_name .
+}
+
+function test(){
+    echo $project_path_prefix
+    path=$(pwd)
+    echo $path
+}
+
+# test
 
 # 入口
 build_image $project_name $sdk_name $service_name
