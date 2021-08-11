250f0cc3eac3677a83979f8381fda2503141829f
yangtzeyufei <yangtze_yufei@163.com>
2021-08-11 21:53:18 +0800

    feat(docker):修改打镜像脚本

--- docker/mariadb/docker_build.sh
+++ docker/mariadb/docker_build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# 项目版本
+# 项目信息
 version="v2.0"
+db_name="auth"
 
 function build_image()
 {
-    echo "进入auth mariadb init制作镜像目录："$(pwd)
+    echo "进入mariadb init制作镜像目录："$(pwd)
 
     latest_commit_id=$(git rev-parse --short HEAD)
     branch=$(git symbolic-ref --short -q HEAD)
     time=$(date "+%Y%m%d_%H%M%S")
     tag=$version"_"$branch"_"$time"_"$latest_commit_id
-    docker_name="auth_mariadb_init:"$tag
+    docker_name=$db_name"_mariadb_init:"$tag
+
     sudo docker build -t $docker_name .
 }
 
